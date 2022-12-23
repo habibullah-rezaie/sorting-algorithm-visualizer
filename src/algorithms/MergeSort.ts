c9684f625f8fd5f95212be768687a6af254774c9
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2022-12-23 14:26:23 +0300

    Create Merge Sort class

---
+++ src/algorithms/MergeSort.ts
@@ -0,0 +1,51 @@
+import { ArrItem } from "./Comparator";
+import { Sort } from "./Sort";
+
+export class MergeSort extends Sort {
+	mergeSortedArrays = (leftArr: ArrItem[], rightArr: ArrItem[]) => {
+		let leftIndex = 0;
+		let rightIndex = 0;
+		const sortedArr = [];
+
+		while (leftIndex < leftArr.length && rightIndex < rightArr.length) {
+			if (
+				this.comparator.lessThanEqual(leftArr[leftIndex], rightArr[rightIndex])
+			) {
+				sortedArr.push(leftArr[leftIndex]);
+				leftIndex++;
+			} else {
+				this.callbacks.compareCallback?.(
+					leftArr[leftIndex].id,
+					rightArr[rightIndex].id,
+					"greater"
+				);
+				this.callbacks.moveCallback?.(
+					rightArr[rightIndex].id,
+					sortedArr.length - (leftArr.length + rightIndex)
+				);
+				sortedArr.push(rightArr[rightIndex]);
+				rightIndex++;
+			}
+		}
+
+		const leftSlice = leftArr.slice(leftIndex);
+		const rightSlice = rightArr.slice(rightIndex);
+
+		const finalArr = [...sortedArr, ...leftSlice, ...rightSlice];
+
+		return finalArr;
+	};
+
+	sort(arr: ArrItem[]): ArrItem[] {
+		if (arr.length <= 1) {
+			return arr;
+		}
+
+		const middle = Math.floor(arr.length / 2);
+
+		return this.mergeSortedArrays(
+			this.sort(arr.slice(0, middle)),
+			this.sort(arr.slice(middle))
+		);
+	}
+}
