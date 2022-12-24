0784f443216b79a7ecf6a3f34e2d41d06363cf8a
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2022-12-24 12:17:14 +0300

    Restrict color and size of bars

--- src/components/Bars.tsx
+++ src/components/Bars.tsx
@@ -9,27 +9,61 @@ function Bars({
 	randomArr: ArrItem[];
 	currentEvent?: SorterEvent;
 }) {
+	const len = randomArr.length;
+	const ulMaxWidth =
+		window.innerWidth - 28 > 872 ? 872 : window.innerWidth - 28; // 28 for 28 px padding around
+	const ulActualWidth =
+		len * 12 - len + 1; /* for gap of 1px and width of 11px*/
+	const liWidth = Math.floor(
+		ulActualWidth > ulMaxWidth ? (ulMaxWidth - len) / len : 12
+	);
+
 	return (
-		<div className="flex flex-row justify-center">
-			<ul className="flex flex-row space-x-1">
-				{randomArr.map((item, i) => (
-					<li
-						className={`w-3 ${
-							!currentEvent
-								? "bg-blue-400"
-								: (currentEvent.type === "compare" ||
-										currentEvent.type === "swap") &&
-								  (item.id === currentEvent.aId || item.id === currentEvent.bId)
-								? "bg-red-400"
-								: "bg-blue-400"
-						}`}
-						style={{ height: `${item.val}px` }}
-						key={item.id}
-					></li>
-				))}
+		<div className="w-full flex justify-center px-2 rotate-180">
+			<ul
+				className={`w-full grid grid-flow-col justify-center`}
+				style={{ columnGap: liWidth === 0 ? "0.5px" : `1px` }}
+			>
+				{randomArr.map((item, i) => {
+					const color = getBarsColor(item, currentEvent);
+					return (
+						<li
+							className={`${color}`}
+							style={{
+								height: `${item.val}px`,
+								width: liWidth === 0 ? "1px" : liWidth + "px",
+							}}
+							key={item.id}
+						></li>
+					);
+				})}
 			</ul>
 		</div>
 	);
+
+	function getBarsColor(item: ArrItem, currentEvent?: SorterEvent) {
+		if (!currentEvent) return "bg-blue-400";
+		switch (currentEvent.type) {
+			case "compare":
+				if (item.id === currentEvent.aId || item.id === currentEvent.bId) {
+					console.log(currentEvent.willSwap);
+					if (currentEvent.willSwap) return "bg-red-400";
+					return "bg-green-400";
+				}
+				break;
+			case "swap":
+				if (item.id === currentEvent.aId || item.id === currentEvent.bId)
+					return "bg-yellow-400";
+				break;
+			case "move":
+				if (currentEvent.id === item.id) return "bg-yellow-400";
+				break;
+			default:
+				break;
+		}
+
+		return "bg-blue-400";
+	}
 }
 
 export default Bars;
